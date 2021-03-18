af6a34826539a41b6b01204422db11bb87064083
zulma <zumendez7@gmail.com>
2021-03-18 23:12:05 +0100

    style navbar et banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
         <div>
             <footer className="footer">
                 <div className="text-center d-flex flex-column ">
-                    <div className="mt-3 d-flex justify-content-center ">
-                        <a className="text-secondary" rel="noopener noreferrer" target='_blank' href="https://www.facebook.com/ztsimi.1"><i className="fab fa-facebook-f fa-3x p-2  ml-2"></i></a>
-                        <a className="text-secondary" rel="noopener noreferrer" target='_blank' href="https://www.instagram.com/mendez_ulma/"><i className="fab fa-instagram fa-3x p-2 ml-2"></i></a>
-                        <a className="text-secondary" rel="noopener noreferrer" target='_blank' href="https://github.com/ZulMendez"><i className="fab fa-github fa-3x p-2  ml-2"></i></a>
+                    <div className="d-flex justify-content-center mt-3">
+                        <a className="text-secondary" href="https://www.facebook.com/ztsimi.1"><i className="fab fa-facebook-f fa-3x p-2  ml-2"></i></a>
+                        <a className="text-secondary" href="https://www.instagram.com/mendez_ulma/"><i className="fab fa-instagram fa-3x p-2 ml-2"></i></a>
+                        <a className="text-secondary" href="https://github.com/ZulMendez"><i className="fab fa-github fa-3x p-2  ml-2"></i></a>
                     </div>
                     <div className="mt-2 text-white">
                         <p className="font-weight-bold text-secondary mr-3">All Rights Reserved || &copy;opyright 2021 || Made with <i className="far fa-heart"></i> by Zulma Mendez</p>
